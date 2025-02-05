8c87e2e3f8bdd2f61e0c1595d27efed484e103bc
ahaseeb235 <ahaseeb235@gmail.com>
2025-02-05 22:26:49 +0000

    added env variables

--- worktimetracker/settings.py
+++ worktimetracker/settings.py
@@ -28,12 +28,12 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-en*f0da2ea*$e#h!ypuck+u5smo#qh)sk#jtbupo8roagxz+d0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'TRUE'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('RENDER_HOSTNAME', '')]
 
 
 
